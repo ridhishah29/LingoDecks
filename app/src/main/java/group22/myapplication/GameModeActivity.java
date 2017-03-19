24c95585b84527d34aa2fd4e74cb2889e36834ec
Vonnie Voong <vonnievoong@yahoo.co.uk>
2017-03-19 13:27:41 +0000

    Link main activity to game menu activity

---
+++ app/src/main/java/group22/myapplication/GameModeActivity.java
@@ -0,0 +1,13 @@
+package group22.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameModeActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //sets the layout for activity
+        setContentView(R.layout.game_mode);
+    }
+}
--- app/src/main/java/group22/myapplication/MainActivity.java
+++ app/src/main/java/group22/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package group22.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity{
 
@@ -10,5 +13,21 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setLanguage(View view){
+        String button_clicked;
+        button_clicked = ((Button) view).getText().toString();
+
+        if(button_clicked.equals("German")){
+            //set language to be german
+            Intent intent = new Intent(this, GameModeActivity.class);
+            startActivity(intent);
+        }
+        else if(button_clicked.equals("Spanish")){
+            //set language to be spanish
+            Intent intent = new Intent(this, GameModeActivity.class);
+            startActivity(intent);
+        }
     }
 }
