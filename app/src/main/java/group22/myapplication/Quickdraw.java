2d532cab78a9eee86745adc167e63c291b1b9124
ridhishah29 <ridhishah29@gmail.com>
2017-05-04 20:40:46 +0100

    Crappy pause screen
    Timer pauses when game is paused

--- app/src/main/java/group22/myapplication/Quickdraw.java
+++ app/src/main/java/group22/myapplication/Quickdraw.java
@@ -1,8 +1,14 @@
 package group22.myapplication;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.CountDownTimer;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,45 +26,58 @@ public class Quickdraw extends Activity {
 
         textView = (TextView) findViewById((R.id.timer));
 
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             countdowntimer = new CountDownTimerClass(60000, 1000);
             countdowntimer.start();
+
         } else {
-            String what = savedInstanceState.getString("key");
-            int whatty = Integer.parseInt(what);
-            countdowntimer = new CountDownTimerClass(whatty*1000,1000);
+            String getTimerState = savedInstanceState.getString("key");
+            int intTimer = Integer.parseInt(getTimerState);
+            countdowntimer = new CountDownTimerClass(intTimer * 1000, 1000);
             countdowntimer.start();
         }
+
+        Button pause = (Button) findViewById(R.id.pause_btn);
+        pause.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(Quickdraw.this);
+                dialog.setContentView(R.layout.pause_dialog);
+                dialog.setTitle("Paused");
+                dialog.setCancelable(true);
+
+                countdowntimer.cancel();
+                dialog.show();
+            }
+        });
     }
 
     @Override
     protected void onSaveInstanceState (Bundle outState) {
         super.onSaveInstanceState(outState);
-        String rf = textView.getText().toString();
-        outState.putString("key", rf);
+        String setTimerState = textView.getText().toString();
+        outState.putString("key", setTimerState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     public class CountDownTimerClass extends CountDownTimer {
 
         public CountDownTimerClass(long millisInFuture, long countDownInterval) {
-
             super(millisInFuture, countDownInterval);
-
         }
 
         @Override
         public void onTick(long millisUntilFinished) {
-
             int progress = (int) (millisUntilFinished/1000);
-
             textView.setText(Integer.toString(progress));
-
         }
 
         @Override
         public void onFinish() {
             textView.setText("Time's up");
-
         }
     }
 
