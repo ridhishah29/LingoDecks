bfeeb314751a425770eed0291b8a37f2a5973058
ridhishah29 <ridhishah29@gmail.com>
2017-05-04 22:37:51 +0100

    Pause screen - resume
    Doesn't dismiss on orientation change
    Clicking resume starts timer
    Notes: Back button doesn't resume timer. Orientation change resumes
    timer even on pause screen.

--- app/src/main/java/group22/myapplication/Quickdraw.java
+++ app/src/main/java/group22/myapplication/Quickdraw.java
@@ -10,6 +10,7 @@ import android.app.Activity;
 import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +19,7 @@ public class Quickdraw extends Activity {
     private Button button;
     TextView textView;
     CountDownTimer countdowntimer;
+    boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +33,47 @@ public class Quickdraw extends Activity {
             countdowntimer.start();
 
         } else {
-            String getTimerState = savedInstanceState.getString("key");
-            int intTimer = Integer.parseInt(getTimerState);
-            countdowntimer = new CountDownTimerClass(intTimer * 1000, 1000);
+            int getTimerState = Integer.parseInt(savedInstanceState.getString("key"));
+            countdowntimer = new CountDownTimerClass(getTimerState * 1000, 1000);
             countdowntimer.start();
         }
+
+
+        //opens custom dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.pause_dialog,null);
+        builder.setView(dialogView);
+        final AlertDialog dialog = builder.create();
 
         Button pause = (Button) findViewById(R.id.pause_btn);
         pause.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                final Dialog dialog = new Dialog(Quickdraw.this);
-                dialog.setContentView(R.layout.pause_dialog);
-                dialog.setTitle("Paused");
-                dialog.setCancelable(true);
-
                 countdowntimer.cancel();
                 dialog.show();
+                Button btnresume = (Button) dialog.findViewById(R.id.btn_resumeGame);
+                btnresume.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View view) {
+                        int pausedcount = Integer.parseInt(textView.getText().toString());
+                        countdowntimer = new CountDownTimerClass(pausedcount*1000, 1000);
+                        countdowntimer.start();
+                        dialog.dismiss();
+                    }
+                });
+
+                keepDialogOpen(dialog);
+
             }
         });
+    }
+
+    // prevent pause screen open when orientation changes
+    private void keepDialogOpen(Dialog dialog) {
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialog.getWindow().setAttributes(lp);
     }
 
     @Override
@@ -56,11 +81,6 @@ public class Quickdraw extends Activity {
         super.onSaveInstanceState(outState);
         String setTimerState = textView.getText().toString();
         outState.putString("key", setTimerState);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
     }
 
     public class CountDownTimerClass extends CountDownTimer {
@@ -73,11 +93,13 @@ public class Quickdraw extends Activity {
         public void onTick(long millisUntilFinished) {
             int progress = (int) (millisUntilFinished/1000);
             textView.setText(Integer.toString(progress));
+            isRunning = true;
         }
 
         @Override
         public void onFinish() {
             textView.setText("Time's up");
+            isRunning = false;
         }
     }
 
