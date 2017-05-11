b75340b92c7532e5dffe811abe61bc624d594b24
MichaelSarpong <m.sarpong-boateng@student.lboro.ac.uk>
2017-05-11 15:00:44 +0100

    Alert Dialog Working!
    Need help correcting the READ EXTERNAL STORAGE and CAMERA Permission
    errors

--- app/src/main/java/group22/myapplication/CreateMenuActivity.java
+++ app/src/main/java/group22/myapplication/CreateMenuActivity.java
@@ -1,50 +1,20 @@
 package group22.myapplication;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import group22.myapplication.R;
 
 public class CreateMenuActivity extends Activity {
-
-    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
-    private Button btnPictureCard;
-    private ImageView ivImage;
-    private String userChosenTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //sets the layout for activity
         setContentView(R.layout.card_creator);
-
-        //Setting the button for the Select Photo Dialog
-        btnPictureCard = (Button) findViewById(R.id.createpic_btn);
-        btnPictureCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectImage();
-            }
-        });
-        ivImage = (ImageView) findViewById(R.id.imageView);
 
         // this can be added wherever the api is needed.
         SharedPreferences langPref = getSharedPreferences("setLanguage", MODE_PRIVATE);
@@ -67,70 +37,15 @@ public class CreateMenuActivity extends Activity {
         }
 
     }
-    
-
-    private void selectImage() {
-
-        //Creating an AlertDialog with 3 items from an array
-        final CharSequence[] items = {"Take Photo", "Choose from Library", "Cancel"};
-        AlertDialog.Builder builder = new AlertDialog.Builder(CreateMenuActivity.this);
-
-        //Setting a title for the Dialog box
-        builder.setTitle("Add Photo!");
-
-        //Choosing the function depending on which button the user clicked
-        builder.setItems(items, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int item) {
 
 
-                if (items[item].equals("Take Photo")) {
-                    userChosenTask = "Take Photo";
-                        cameraIntent();
 
-                } else if (items[item].equals("Choose from Library")) {
-                    userChosenTask = "Choose from Library";
-                        galleryIntent();
-
-                } else if (items[item].equals("Cancel")) {
-                    dialog.dismiss();
-                }
-            }
-        });
-
-        builder.show();
-    }
-
-    private void galleryIntent()
-    {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);//
-        startActivityForResult(Intent.createChooser(intent, "Select File"),SELECT_FILE);
-    }
-
-    private void cameraIntent()
-    {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, REQUEST_CAMERA);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == SELECT_FILE)
-                onSelectFromGalleryResult(data);
-            else if (requestCode == REQUEST_CAMERA)
-                onCaptureImageResult(data);
-        }
-    }
-
+    /*
     private void onCaptureImageResult(Intent data) {
-        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        Bundle extras = data.getExtras();
+        Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
 
         File destination = new File(Environment.getExternalStorageDirectory(),
                 System.currentTimeMillis() + ".jpg");
@@ -147,10 +62,9 @@ public class CreateMenuActivity extends Activity {
             e.printStackTrace();
         }
 
-        ivImage.setImageBitmap(thumbnail);
+        ivImage.setImageBitmap(imageBitmap);
     }
 
-    @SuppressWarnings("deprecation")
     private void onSelectFromGalleryResult(Intent data) {
 
         Bitmap bm=null;
@@ -163,6 +77,6 @@ public class CreateMenuActivity extends Activity {
         }
 
         ivImage.setImageBitmap(bm);
-    }
+    }*/
 }
 
--- app/src/main/java/group22/myapplication/CreatePicActivity.java
+++ app/src/main/java/group22/myapplication/CreatePicActivity.java
@@ -1,21 +1,153 @@
 package group22.myapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CreatePicActivity extends Activity{
+
+    private Button btnPictureCard;
+    private ImageView ivImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //sets the layout for activity
         setContentView(R.layout.picture_card);
 
+        //Setting the button for the Select Photo Dialog
+        btnPictureCard = (Button) findViewById(R.id.choosepic_btn);
+        ivImage = (ImageView) findViewById(R.id.imageView);
+        btnPictureCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
         // this can be added wherever the api is needed.
         SharedPreferences langPref = getSharedPreferences("setLanguage", MODE_PRIVATE);
         String languageSet = langPref.getString("language", "");
         Log.v("language chosen", languageSet);
     }
+
+    private void selectImage() {
+
+        //Creating an AlertDialog with 3 items from an array
+        final CharSequence[] items = {"Take Photo", "Choose from Library", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(CreatePicActivity.this);
+
+        //Setting a title for the Dialog box
+        builder.setTitle("Add Photo!");
+
+        //Choosing the function depending on which button the user clicked
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+
+                if (items[item].equals("Take Photo")) {
+                    //userChosenTask = "Take Photo";
+                    dispatchTakePictureIntent();
+
+                } else if (items[item].equals("Choose from Library")) {
+                    //userChosenTask = "Choose from Library";
+                    galleryIntent();
+
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        builder.show();
+    }
+
+
+    private void galleryIntent() {
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhoto , 1);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePicture, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    ivImage.setImageURI(selectedImage);
+                }
+
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    ivImage.setImageURI(selectedImage);
+                }
+                break;
+        }
+    }
+
+    /*
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ivImage.setImageBitmap(thumbnail);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+
+        Bitmap bm=null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        ivImage.setImageBitmap(bm);
+    }*/
 
 }
